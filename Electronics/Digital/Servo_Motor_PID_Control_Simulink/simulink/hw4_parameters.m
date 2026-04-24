a94f5743450175861ff8a93e28d4f56b24c13d89
elifsudeztrk03 <elifsudeztrk03@gmail.com>
2026-04-24 19:11:53 +0300

    edit pid

---
+++ Electronics/Digital/Servo_Motor_PID_Control_Simulink/simulink/hw4_parameters.m
@@ -0,0 +1,9 @@
+J = 0.005;
+B = 0.6;
+Kt = 0.08;
+
+theta_desired = pi;   % 180 degree
+theta0 = 0;           % initial position
+sim_time = 0.5;       
+K1 = Kt / J;          % 16
+K2 = B / J;           % 120
